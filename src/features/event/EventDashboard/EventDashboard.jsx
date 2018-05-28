be9f16698951255f3c98c8c701d5636e6873e212
Brian Bycynski <high5bri@hotmail.com>
2018-05-28 12:16:13 -0400

    adding firebase next, finished section 13

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -4,6 +4,7 @@ import { Grid } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import { deleteEvent } from '../eventActions';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
+import EventActivity from '../EventActivity/EventActivity';
 const mapState = state => ({
     events: state.events,
     loading: state.async.loading
@@ -29,7 +30,9 @@ class EventDashboard extends Component {
                         events={events}
                     />
                 </Grid.Column>
-                <Grid.Column width={6} />
+                <Grid.Column width={6}>
+                    <EventActivity />
+                </Grid.Column>
             </Grid>
         );
     }
